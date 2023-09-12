838f8ec3c9cc3b7c91082c025065df6b3abfbd5a
UmaidJawed <umaidjawed2002@gmail.com>
2023-09-12 22:41:42 +0530

    Create Pattern1.java
    * * * * * * *
    * * * * * * *
    * * * * * * *
    * * * * * * *
    * * * * * * *
    * * * * * * *
    * * * * * * *

---
+++ Pattern_Questions/Pattern1.java
@@ -0,0 +1,15 @@
+package Pattern_Questions;
+
+import java.util.Scanner;
+
+public class Pattern1 {
+    public static void nForest(int n) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
