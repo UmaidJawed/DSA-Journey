e95fd1b3f972565b35734e19cd148486f679044d
UmaidJawed <umaidjawed2002@gmail.com>
2023-09-18 22:57:12 +0530

    Reverse Array

---
+++ Recursion/Reverse_Array.java
@@ -0,0 +1,10 @@
+public class Reverse_Array {
+    public static int[] reverseArray(int n, int[] nums) {
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = nums[n - 1 - i];
+        }
+        return arr;
+
+    }
+}
