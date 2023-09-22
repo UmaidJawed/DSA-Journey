86fa95675c9d1a485cf9556e61f7c35a803ca1f4
UmaidJawed <umaidjawed2002@gmail.com>
2023-09-22 10:19:27 +0530

    init

--- Sorting/Selection_Sort.java
+++ Sorting/Selection_Sort.java
@@ -7,7 +7,7 @@ public class Selection_Sort {
 
         for (int i = 0; i <= n - 2; i++) {
             int mini = i;
-            for (int j = 0; j <= n - 1; j++) {
+            for (int j = i; j <= n - 1; j++) {
                 if (arr[j] < arr[mini]) {
                     mini = j;
                 }
