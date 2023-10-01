fb48cc01b3ca72cba058792540e52e8cdaefb141
UmaidJawed <umaidjawed2002@gmail.com>
2023-10-01 23:45:15 +0530

    Checking_if_array_sorted

---
+++ Arrays/Check_Array_Sorted.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class Check_Array_Sorted {
+    public static int isSorted(int n, int[] a) {
+        for (int i = 0; i < n - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
