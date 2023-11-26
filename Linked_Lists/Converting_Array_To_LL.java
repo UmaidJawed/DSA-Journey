11acfcb9ef559ba4f580b302d869e07d49208ffa
UmaidJawed <umaidjawed2002@gmail.com>
2023-11-27 01:23:08 +0530

    init

--- Linked_Lists/Converting_Array_To_LL.java
+++ Linked_Lists/Converting_Array_To_LL.java
@@ -22,7 +22,7 @@ public class Converting_Array_To_LL {
         Node head = new Node(arr[0]);
         Node mover = head;
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             Node temp = new Node(arr[i]);
             mover.next = temp;
             mover = temp;
