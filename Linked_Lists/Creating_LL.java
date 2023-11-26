e8efd936af6e53973f71ee076a6cedae2c00214b
UmaidJawed <umaidjawed2002@gmail.com>
2023-11-27 00:40:27 +0530

    Basic Of Linked List
    in this we are learning how to create a linked list manually

---
+++ Linked_Lists/Creating_LL.java
@@ -0,0 +1,26 @@
+package Linked_Lists;
+
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data1, Node next1) {
+        this.data = data1;
+        this.next = next1;
+    }
+
+    Node(int data1) {
+        this.data = data1;
+        this.next = null;
+    }
+}
+
+public class Creating_LL {
+    public static void main(String[] args) {
+        int[] arr = { 2, 5, 8, 7 };
+        Node y = new Node(arr[0]);
+        System.out.println(y.data);
+    }
+}
