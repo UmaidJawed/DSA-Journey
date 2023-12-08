2b269cd71354bc049a2a8469e0d8cdcdde99afdb
UmaidJawed <umaidjawed2002@gmail.com>
2023-12-09 00:04:22 +0530

    init

--- Linked_Lists/Deleting_kth_Value.java
+++ Linked_Lists/Deleting_kth_Value.java
@@ -6,26 +6,19 @@ class Node {
     int data;
     Node next;
 
-    Node(int data1, Node next1) {
-        this.data = data1;
-        this.next = next1;
-    }
-
     Node(int data1) {
         this.data = data1;
         this.next = null;
     }
 }
 
-public class Deletion_Of_Tail {
-    private static Node convertArr2LL(int[] arr) {
+public class Deleting_kth_Value {
+    private static Node convertArr2LL(int arr[]) {
         Node head = new Node(arr[0]);
         Node mover = head;
-
         for (int i = 1; i < arr.length; i++) {
             Node temp = new Node(arr[i]);
             mover.next = temp;
-            mover = temp;
         }
         return head;
     }
@@ -49,16 +42,5 @@ public class Deletion_Of_Tail {
         }
         temp.next = null;
         return head;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = { 12, 5, 6, 8 };
-        Node head = convertArr2LL(arr);
-        Node temp = head;
-        head = deleteTail(head);
-        while (temp != null) {
-            System.out.print(temp.data + "-->" + "");
-            temp = temp.next;
-        }
     }
 }
