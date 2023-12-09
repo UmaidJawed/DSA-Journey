cee9346ace5abfd913926f2c1a39a5018a7cf259
UmaidJawed <umaidjawed2002@gmail.com>
2023-12-10 00:52:14 +0530

    Inserting new node at the tail position

---
+++ Linked_Lists/Inserting_Node_At_Tail_In_LL.java
@@ -0,0 +1,120 @@
+package Linked_Lists;
+
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data1) {
+        this.data = data1;
+        this.next = null;
+    }
+}
+
+public class Inserting_Node_At_Tail_In_LL {
+    private static Node convertArr2LL(int arr[]) {
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for (int i = 1; i < arr.length; i++) {
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+        }
+        return head;
+    }
+
+    // Function for deleting head in the Linked Lists
+    private static Node deleteHead(Node head) {
+        Node temp = head;
+        if (head == null)
+            return head;
+        head = head.next;
+        return head;
+    }
+
+    private static Node deleteTail(Node head) {
+        if (head == null || head.next == null)
+            return null;
+
+        Node temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+        temp.next = null;
+        return head;
+    }
+
+    private static Node deletekElement(Node head, int k) {
+        if (head == null)
+            return null;
+
+        if (k == 1) {
+            head = head.next;
+            return head;
+        }
+        int cnt = 0;
+        Node temp = head;
+        Node prev = null;
+        while (temp != null) {
+            cnt++;
+            if (cnt == k) {
+                prev.next = prev.next.next;
+                break;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+        return head;
+    }
+
+    private static Node deleteValue(Node head, int ele) {
+        if (head == null)
+            return null;
+
+        if (head.data == ele) {
+            head = head.next;
+            return head;
+        }
+        Node temp = head;
+        Node prev = null;
+        while (temp != null) {
+            if (temp.data == ele) {
+                prev.next = prev.next.next;
+                break;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+        return head;
+    }
+
+    private static Node insertHead(Node head, int val) {
+        Node temp = new Node(val);
+        temp.next = head;
+        return temp;
+    }
+
+    private static Node insertTail(Node head, int val) {
+        if (head == null)
+            return new Node(val);
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        Node newnode = new Node(val);
+        temp = newnode;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 12, 5, 6, 8 };
+        Node head = convertArr2LL(arr);
+        head = insertTail(head, 100);
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data + "-->" + "");
+            temp = temp.next;
+        }
+
+    }
+}
