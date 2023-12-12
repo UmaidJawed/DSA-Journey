3deea1d685403d98ac3c2176d7b47c82fed326e6
UmaidJawed <umaidjawed2002@gmail.com>
2023-12-13 01:01:13 +0530

    Debugged Code

--- Linked_Lists/Inserting_Node_At_Tail_In_LL.java
+++ Linked_Lists/Inserting_Node_At_Tail_In_LL.java
@@ -24,6 +24,7 @@ public class Inserting_Node_At_Tail_In_LL {
         for (int i = 1; i < arr.length; i++) {
             Node temp = new Node(arr[i]);
             mover.next = temp;
+            mover = temp;
         }
         return head;
     }
