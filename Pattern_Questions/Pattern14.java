20f6f1e9ff58f24eefc46618a6af60e9eb9c1e04
UmaidJawed <umaidjawed2002@gmail.com>
2023-12-17 23:55:28 +0530

    Increasing Letter Triangle Pattern

---
+++ Pattern_Questions/Pattern14.java
@@ -0,0 +1,13 @@
+package Pattern_Questions;
+
+// Increasing Letter Triangle Pattern
+public class Pattern14 {
+    public static void nLetterTriangle(int n) {
+        for (int i = 0; i < n; i++) {
+            for (char ch = 'A'; ch <= 'A' + i; ch++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+}
