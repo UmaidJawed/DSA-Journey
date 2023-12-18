ddf5d86c06e0331b1b44f140e632233cd525dbac
UmaidJawed <umaidjawed2002@gmail.com>
2023-12-18 23:59:37 +0530

    Create Pattern17.java

---
+++ Pattern_Questions/Pattern17.java
@@ -0,0 +1,38 @@
+package Pattern_Questions;
+
+import java.util.Scanner;
+
+public class Pattern17 {
+    public static void alphaHill(int n) {
+        for (int i = 0; i < n; i++) {
+            // spaces
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            // characters
+            char ch = 'A';
+            int breakpoint = (2 * i + 1) / 2;
+            for (int j = 1; j <= 2 * i + 1; j++) {
+                System.out.print(ch + " ");
+
+                if (j <= breakpoint)
+                    ch++;
+
+                else
+                    ch--;
+            }
+
+            // spaces
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        alphaHill(n);
+    }
+}
