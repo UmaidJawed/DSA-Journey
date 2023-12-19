93b9ac41b43408ef95dc5386a46406be8d365089
UmaidJawed <umaidjawed2002@gmail.com>
2023-12-19 23:53:26 +0530

    Create Pattern21.java

---
+++ Pattern_Questions/Pattern21.java
@@ -0,0 +1,16 @@
+package Pattern_Questions;
+
+public class Pattern21 {
+    public static void getStarPattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 || i == n - 1 || j == n - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
