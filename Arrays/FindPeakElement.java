b323d0c90f60d4807d4654615d80b4e895966593
UmaidJawed <umaidjawed2002@gmail.com>
2024-07-28 20:30:06 +0530

    Find The Peak Element
    Using BruteForce Approach in which nums{i-1]<nums[i] And nums[i]>nums[i+1]

---
+++ Arrays/FindPeakElement.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+//Using Brute-Force Approach
+
+public class FindPeakElement {
+    public int findPeakElement(int[] nums) {
+
+        int n = nums.length;
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+            if ((i == 0 || nums[i - 1] < nums[i]) && (i == n - 1 || nums[i] > nums[i + 1])) {
+                return i;
+            }
+        }
+
+        return -1;
+}
